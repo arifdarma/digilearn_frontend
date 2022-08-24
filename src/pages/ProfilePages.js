79117d43d76da7860341f22d5187f219bf94829a
arif.althia <arif.althia@shopee.com>
2022-08-24 14:47:54 +0700

    feat: add list voucher and gift on profile page

--- src/pages/ProfilePages.js
+++ src/pages/ProfilePages.js
@@ -11,6 +11,8 @@ function ProfilePages() {
   const MyAlert = withReactContent(Swal);
   const [error, setError] = useState('');
   const [status, setStatus] = useState(200);
+  const [voucher, setVoucher] = useState([]);
+  const [gift, setGift] = useState([]);
   const [user, setUser] = useState({
     name: '',
     userName: '',
@@ -23,31 +25,57 @@ function ProfilePages() {
   });
 
   useEffect(() => {
-    const getProfile = 'http://localhost:8080/users';
-    fetch(getProfile, {
+    const getProfile = fetch('http://localhost:8080/users', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
       body: null,
-    }).then((response) => {
-      if (!response.ok) {
-        return response.text().then((text) => { throw new Error(text); });
-      }
-      setStatus(response.status);
-      return response.json();
-    }).then((data) => {
-      setUser({
-        name: data.data.name,
-        userName: data.data.user_name,
-        email: data.data.email,
-        phoneNo: data.data.phone_no,
-        address: data.data.address,
-        referralCode: data.data.referralCode,
-        level: data.data.level,
-      });
-    })
+    });
+    const getVoucher = fetch('http://localhost:8080/vouchers', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      body: null,
+    });
+    const getGift = fetch('http://localhost:8080/users-gifts', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      body: null,
+    });
+
+    Promise.all([getProfile, getVoucher, getGift])
+      .then(([resProfile, resVoucher, resGift]) => Promise.all(
+        [resProfile.json(), resVoucher.json(), resGift.json()],
+      ))
+      .then(([dataProfile, dataVoucher, dataGift]) => {
+        if (dataProfile.statusCode !== 200) {
+          throw new Error(dataProfile);
+        }
+        if (dataVoucher.statusCode !== 200) {
+          throw new Error(dataVoucher);
+        }
+        if (dataGift.statusCode !== 200) {
+          throw new Error(dataGift);
+        }
+        setUser({
+          name: dataProfile.data.name,
+          userName: dataProfile.data.user_name,
+          email: dataProfile.data.email,
+          phoneNo: dataProfile.data.phone_no,
+          address: dataProfile.data.address,
+          referralCode: dataProfile.data.referralCode,
+          level: dataProfile.data.level,
+        });
+        setVoucher(dataVoucher.data);
+        setGift(dataGift.data);
+      })
       .catch((err) => {
         setError(JSON.parse(err.message).message);
         setStatus(JSON.parse(err.message).statusCode);
@@ -81,6 +109,31 @@ function ProfilePages() {
       <div>{user.address}</div>
       <div>{user.referralCode}</div>
       <div>{user.level}</div>
+      <h1>Voucher</h1>
+      <div>
+        {
+          voucher.map((userVoucher) => (
+            <div className="row border my-3" id={userVoucher.id}>
+              <p className="col">{userVoucher.code}</p>
+              <p className="col">{userVoucher.value}</p>
+              <p className="col">{userVoucher.expired_date.split('T')[0]}</p>
+              <p className="col">{userVoucher.status}</p>
+            </div>
+          ))
+        }
+      </div>
+      <h1>Gift</h1>
+      <div>
+        {
+                gift.map((userGift) => (
+                  <div className="row border my-3" id={userGift.id}>
+                    <p className="col">{userGift.gift_name}</p>
+                    <p className="col">{userGift.status}</p>
+                    <p className="col">{userGift.date_estimated.split('T')[0]}</p>
+                  </div>
+                ))
+            }
+      </div>
     </>
   );
 }
